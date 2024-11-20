a8ef980ea4ab4bd3e9cb1dc96ab2ae54fcbdeb85
ourdearleadereason <ourdearleadereason@gmail.com>
2024-11-20 16:32:37 -0600

    built frontend for login/register, database logic to be implemented

--- app/src/main/java/com/cs407/project/MainActivity.kt
+++ app/src/main/java/com/cs407/project/MainActivity.kt
@@ -11,10 +11,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        val fragmentManager=supportFragmentManager
+        fragmentManager.beginTransaction()
+            .replace(R.id.startfragment, StartFragment::class.java, null)
+            .setReorderingAllowed(true)
+            .addToBackStack("loading start fragment")
+            .commit()
     }
+
 }

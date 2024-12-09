77e884d5652943930f4b7050c01cbb7abd1e41b4
Yiru WU <wu754@wisc.edu>
2024-12-08 21:04:47 -0600

    Add ReviewDatabase

--- app/src/main/java/com/cs407/project/data/AppDatabase.kt
+++ app/src/main/java/com/cs407/project/data/AppDatabase.kt
@@ -73,3 +73,25 @@ abstract class MessagesDatabase : RoomDatabase() {
     }
 }
 
+@Database(entities = [Review::class], version = 1, exportSchema = false)
+abstract class ReviewDatabase : RoomDatabase() {
+    abstract fun reviewDao(): ReviewDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+
+        fun getInstance(context: Context): AppDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "app_database"
+                ).build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+}
+
--- app/src/main/java/com/cs407/project/data/ReviewDao.kt
+++ app/src/main/java/com/cs407/project/data/ReviewDao.kt
@@ -1,2 +1,17 @@
 package com.cs407.project.data
 
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+@Dao
+interface ReviewDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertReview(review: Review)
+
+    @Query("SELECT * FROM reviews WHERE sellerId = :sellerId AND buyerId = :buyerId LIMIT 1")
+    suspend fun getReviewBySellerAndBuyer(sellerId: Int, buyerId: Int): Review?
+}
+
+
